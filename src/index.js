763c047aab9f64b43a544db104da4f54132951b0
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-01 18:11:56 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById('create-task-form'); //const form = document.getElementById('create-task-form');
+  const list = document.getElementById('tasks');
+  //<div id="list">
+    //<h2>My Todos</h2>
+    //<ul id="tasks"> </ul>
+  //</div>
+  
+  form.addEventListener('submit', addTask); //add task
+
+  list.addEventListener('click', removeTask); //delete
+
+  function addTask(e){
+    e.preventDefault();
+
+    const newTask = document.getElementById('new-task-description').value;
+    console.log(newTask);
+
+    // Create new li element
+    const li = document.createElement('li');
+
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newTask));
+
+    // create delete button element
+    const deleteBtn = document.createElement('button');
+
+    // append text node to delete button
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // append button to li
+    li.appendChild(deleteBtn);
+
+    // Append li to list
+    list.appendChild(li);
+
+  }
+
+  function removeTask(e){
+    if (e.target.nodeName === 'BUTTON') {
+      if(confirm('Are you sure?')) {
+        let li = e.target.parentElement;
+        list.removeChild(li);
+      }
+    }
+
+  }
+
 });
