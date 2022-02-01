3a163d8886a6ae07c27f5594fb394663943643d5
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-01 17:46:45 -0500

    commit tree 6426c5919b8862e3c7ab0f35358e8cc2adbe69fa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById('create-task-form'); //const form = document.getElementById('create-task-form');
+  const list = document.getElementById('tasks');
+  //<div id="list">
+    //<h2>My Todos</h2>
+    //<ul id="tasks"> </ul>
+  //</div>
+  
+  
+
+
 });
